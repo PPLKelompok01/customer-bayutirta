0f8603be3ae7ddae72e64e1f83054d6878f710ab
devissvtr <lkdevi16@gmail.com>
2025-05-30 17:09:37 +0700

    git commit -m "PBI-49 <edit style>"

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -190,7 +190,7 @@
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" style="margin-right: 5px;">
                   <path d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                 </svg>
-                Mohon masukkan Nomor Invoice atau ID Service yang valid
+                Msukkan Nomor Invoice atau ID Service yang valid
               </div>
             </div>
 
